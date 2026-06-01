50515d57de079718c039ac0d10b9b5389bdb5327
Muhammadsid2011 <muhammadsid2011@gmail.com>
2026-06-02 00:20:34 +0500

    fix(schema): update updatedAt field to include onUpdate for users and products tables

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -7,7 +7,10 @@ export const users = pgTable("users", {
     name: text("name"),
     imageUrl: text("image_url"),
     createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
-    updatedAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow()
+    updatedAt: timestamp("updated_at", { mode: "date" })
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => (new Date()))
 })
 
 export const products = pgTable("products", {
@@ -18,7 +21,10 @@ export const products = pgTable("products", {
     userId: text("user_id").notNull()
         .references(() => users.id, { onDelete: "cascade" }),
     createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
-    updatedAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow()
+    updatedAt: timestamp("updated_at", { mode: "date" })
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => (new Date()))
 })
 
 export const comments = pgTable("comments", {
